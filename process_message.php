ad312c7fd6f5ec4eceeb6c6c91d00d21f3b0ba22
DandyTheWood <115947785+DandyTheWood@users.noreply.github.com>
2025-03-02 00:41:07 +0100

    Updated App password for SMTP server

--- process_message.php
+++ process_message.php
@@ -21,7 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
         $mail->Username = 'webserverrpj@gmail.com';
-        $mail->Password = 'okbj ipud uaal eybb';
+        $mail->Password = 'lvqg kztz lqhe pwov';
         $mail->SMTPSecure = 'tls';
         $mail->Port = 587;
 
